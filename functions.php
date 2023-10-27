7c4ac498d80e615b2e112979738453d76a441b8c
Thanh Trần <42226341+ging-dev@users.noreply.github.com>
2023-10-27 22:00:40 +0700

    feat: support symfony webpack encore (#4)
    * feat: support symfony webpack encore
    
    * chore: cs-fix & reduce psalm to level 4
    
    * chore: trim trailing whitespace

--- functions.php
+++ functions.php
@@ -1,4 +1,7 @@
 <?php
+use BenTools\WebpackEncoreResolver\AssetPathResolver;
+
+require_once get_template_directory() . '/vendor/autoload.php';
 
 add_action(
 	'after_setup_theme',
@@ -18,5 +21,28 @@ add_action(
 				'script',
 			)
 		);
+	}
+);
+
+
+/**
+ * Fires when scripts and styles are enqueued.
+ */
+add_action(
+	'wp_enqueue_scripts',
+	function (): void {
+		$assetPathResolver = new AssetPathResolver( get_template_directory() . '/build' );
+
+		$resolve = function ( string $uri ): string {
+			return ( str_starts_with( $uri, '/' ) ? get_stylesheet_directory_uri() : '' ) . $uri;
+		};
+
+		foreach ( $assetPathResolver->getWebpackCssFiles( 'main' ) as $index => $css ) {
+			wp_enqueue_style( "style_{$index}", $resolve( $css ) );
+		}
+
+		foreach ( $assetPathResolver->getWebpackJsFiles( 'main' ) as $index => $js ) {
+			wp_enqueue_script( "script_{$index}", $resolve( $js ) );
+		}
 	}
 );
